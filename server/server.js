2f9ab6efcbc872d088f1d2da05f10cef826e104b
iamalihaider515 <155340437+iamalihaider515@users.noreply.github.com>
2026-03-28 10:42:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
@@ -11,7 +10,6 @@ dotenv.config();
 const app = express();
 
 // Middleware
-//app.use(cors());
 app.use(cors({
   origin: "*"
 }));
@@ -25,15 +23,7 @@ app.use("/api/posts", postRoutes);
 // PORT (Render uses this)
 const PORT = process.env.PORT || 5000;
 
-// DB Connection + Start Server
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB Connected");
-
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
-})
-.catch(err => {
-    console.log("DB Error:", err);
+// ✅ START SERVER (NO DB)
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
